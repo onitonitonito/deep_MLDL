9065999974f2b560ff9d8feccb3b7ae2fbbd778f
onito3 on Earth <nitt0x0@gmail.com>
2018-06-21 13:05:31 +0900

    1.22-modif wd_cloud & some

--- kaggle_pandas.py
+++ kaggle_pandas.py
@@ -1,3 +1,6 @@
+import _script_run_utf8
+_script_run_utf8.main()
+
 import numpy as np          # linear algebra
 import pandas as pd         # data processing, CSV file I/O (e.g. pd.read_csv)
 
@@ -5,10 +8,10 @@ import os
 import sys
 
 # from os.path import dirname, exists
-ROOT = 'four_pillars_of_oop\\'
+ROOT = 'deep_MLDL'
 DIRS = os.path.dirname(__file__).partition(ROOT)
 ROOT_DIR = DIRS[0] + DIRS[1]
 
 print(ROOT_DIR+"_static")
-for i, filename in enumerate(os.listdir(ROOT_DIR+"_static"), 1):
+for i, filename in enumerate(os.listdir(os.path.join(ROOT_DIR, "_static")), 1):
     print("%s. %s"%(i, filename))
--- word_cloud.py
+++ word_cloud.py
@@ -1,9 +1,13 @@
+# 워드클라우드 깃허브에 예제로 잘 올라와 있다
+# https://amueller.github.io/word_cloud/auto_examples/masked.html
+
+
 # %matplotlib inline
 import os
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 
-FILENAME_WITH_DIR = './_static/i_have_a_dream.pdb'
+FILENAME_WITH_DIR = './_static/_made_static/i_have_a_dream.pdb'
 
 f = open(FILENAME_WITH_DIR).read()
 wc = WordCloud(max_font_size=35).generate(f)
@@ -35,13 +39,13 @@ https://amueller.github.io/word_cloud/auto_examples/masked.html
 # # Read the whole f.
 # f = open(path.join(d, 'alice.txt')).read()
 
-DESTIN_DIR = './_static/'
+DESTIN_DIR = './_static/_made_static/'
 
 f = open(FILENAME_WITH_DIR).read()
 
 # read the mask image taken from
 # http://www.stencilry.org/stencils/movies/alice%20in%20wonderland/255fk.jpg
-alice_mask = np.array(Image.open(DESTIN_DIR + "alice_mask.png"))
+alice_mask = np.array(Image.open(DESTIN_DIR + "mask_teapot.png"))
 
 stopwords = set(STOPWORDS)
 stopwords.add("said")
@@ -53,7 +57,7 @@ wc = WordCloud(background_color="white", max_words=2000, mask=alice_mask,
 wc.generate(f)
 
 # store to file
-wc.to_file(DESTIN_DIR + "alice.png")
+wc.to_file(DESTIN_DIR + "wcloud_result_image.png")
 
 # show 1
 plt.imshow(wc, interpolation='bilinear')
